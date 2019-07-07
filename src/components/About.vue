6faf0766eb69864922033cda93705f3436cd0e23
danialfaisal <dfaisal@unomaha.edu>
2019-07-07 16:06:00 -0500

    Initial Commit

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>Danial Faisal's About Page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      their_name: this.$route.params.name
+    };
+  }
+};
+</script>
